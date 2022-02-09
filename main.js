4991355dadeaae77b43c77d21bc4fe2e710ff2be
Andrew Elliott <andrewelliott159@gmail.com>
2022-02-09 14:59:52 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+const residentsButton = document.getElementById("residents-button");
+const namesSection = document.getElementById("names");
+
+function onClick() {
+    axios.get("https://swapi.dev/api/planets/2")
+        .then((res) => {
+            namesSection.innerHTML = "";
+            const residents = res.data.residents;
+
+            for(let i = 0; i < residents.length; i++) {
+                axios.get(residents[i])
+                    .then((res) => {
+                        const link = document.createElement("a");
+                        const nameHeading = document.createElement("h2");
+                        
+                        link.setAttribute("href", residents[i]);
+                        nameHeading.textContent = res.data.name;
+                        
+                        link.appendChild(nameHeading);
+                        namesSection.appendChild(link);
+                    });
+            }
+        });
+}
+
+residentsButton.addEventListener("click", onClick);
